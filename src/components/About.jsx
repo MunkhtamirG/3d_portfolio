8033575e71272a71c51b98f17b6b4c959ef3dd08
MBANK\munkhtamir.g <munkhtamir.g@m-bank.mn>
2023-03-04 11:46:21 +0800

    [ADD] dependecies

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return <section className="relative w-full h-screen mx-auto"></section>;
 };
 
 export default About;
